d63e0feb45389cdc976a3cbfc2f0012f90d2490a
Tomas Gomez <tomasg1223@outlook.com>
2024-06-25 19:23:40 -0300

    inicio de pagina

---
+++ src/components/Cards.jsx
@@ -0,0 +1,8 @@
+
+export function Cards () {
+    return (
+        <section className="bg-white flex items-center justify-center p-4">
+            <h2 className="text-3xl">Productos</h2>
+        </section>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+
+export function Navbar({ img }) {
+    return (
+        <nav className="text-black flex justify-around p-4 bg-white">
+            <div>
+                <h1 className="text-xl">Minecraft Flowers Emporium</h1>
+            </div>
+            <div className="flex items-center gap-4">
+                <input type="text" placeholder="Buscar..." className="rounded-3xl bg-slate-200 p-2" />
+                <img src={img} className="size-7" alt={"Foto de" + img } />
+            </div>
+        </nav>
+    )
+}
